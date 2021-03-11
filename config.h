e90c4a2f599511d0492622dff617e83932c53cdb
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2021-03-11 14:51:22 +0100

    Added ligatures and fixed font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
