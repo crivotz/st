dde2efc15c02a3eba50c98e39705e149d1b8e3c2
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2021-04-12 10:32:14 +0200

    Increase font-size. Fixed 1px vertical problem

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font Mono:pixelsize=13:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
